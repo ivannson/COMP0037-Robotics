80521418bcbe2b67e05987498075c6a92be6e4f8
miraclecodes <miracleabiola4@gmail.com>
2019-01-20 22:48:22 +0000

    working movement

--- lab2sol.py
+++ lab2sol.py
@@ -38,6 +38,10 @@ class Robot:
         self.theta = 0
         self.x = 0
         self.y = 0
+
+        self.distance_tolerance = 0.1
+        self.angle_tolerance = 5
+
         self.rate = rospy.Rate(10)
 
 
@@ -49,7 +53,6 @@ class Robot:
         self.theta = 2 * atan2(self.pose.orientation.z, self.pose.orientation.w) * 180 / pi
         self.x = round(self.pose.position.x, 4)
         self.y = round(self.pose.position.y, 4)
-        print(self.x,self.y)
 
     def euclidean_distance(self,x,y):
         """Euclidean distance between current pose and the goal."""
@@ -71,17 +74,16 @@ class Robot:
     def move2goal(self, waypoint):
         """Moves the turtle to the goal."""
 
+
         # Get the input from the user.
         x = int(waypoint[0])
         y = int(waypoint[1])
 
-        distance_tolerance = 0.1
-        angle_tolerance = 5
 
         vel_msg = Twist()
 
-        while self.euclidean_distance(x,y) >= distance_tolerance:
-            print(self.euclidean_distance(x,y))
+        while self.euclidean_distance(x,y) >= self.distance_tolerance or self.steering_angle >= self.angle_tolerance:
+            print(self.linear_vel(x,y))
 
             # Porportional controller.
             # https://en.wikipedia.org/wiki/Proportional_control
@@ -94,17 +96,18 @@ class Robot:
             # Angular velocity in the z-axis.
             vel_msg.angular.x = 0
             vel_msg.angular.y = 0
-            vel_msg.angular.z = 0
+            vel_msg.angular.z = self.angular_vel(x,y)
             #vel_msg.angular.z = self.angular_vel(x,y)
 
             # Publishing our vel_msg
+            print(vel_msg)
             self.velocity_publisher.publish(vel_msg)
 
             # Publish at the desired rate.
             self.rate.sleep()
 
-        rospy.loginfo('Current position, x: {}, y:{}, theta:{}'.format(self.pose.position.x,
-                self.pose.position.y, self.theta))
+        rospy.loginfo('Current position, x: {}, y:{}, theta:{}'.format(self.x,
+                self.y, self.theta))
 
         rospy.spin()
 
@@ -118,12 +121,10 @@ class Robot:
 if __name__ == '__main__':
     try:
         x = Robot()
-        # for waypoint in waypoints.values():
-        #     x.move2goal(waypoint)
-        #     rospy.spin()
-        #     rospy.sleep(1)
-        waypoint_1 = waypoints[1]
-        x.move2goal(waypoint)
+        for waypoint in waypoints.values():
+            x.move2goal(waypoint)
+            rospy.sleep(1)
+
     except rospy.ROSInterruptException:
         pass
 
