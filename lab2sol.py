78a6990ce30d5ba370107569886394bacd04309d
MoJabeen <jabeen1998@gmail.com>
2019-01-22 16:50:35 +0000

    meh

--- lab2sol.py
+++ lab2sol.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 
+#Import sys to be able to parse command line arguments and read waypoints
 import sys
+
+#Import rospy, message types and math for calculations
 import rospy
 from geometry_msgs.msg import Twist, Pose
 from nav_msgs.msg import Odometry
-
 from math import pow, atan2, sqrt, pi
 
-
+# Create a dictionary to store the x, y and rotation values for each waypoint as a dictionary
+# Waypoints are numbered in the order they appear in the .txt file
 waypoints = {}
 
 with open(sys.argv[1]) as f:
@@ -18,113 +21,132 @@ with open(sys.argv[1]) as f:
             waypoints[counter] = waypoint
             counter +=1
 
-class Robot:
+class Robot_controller:
 
     def __init__(self):
-        # Creates a node with name 'turtlebot_controller' and make sure it is a
+        # Creates a node with name 'Robot_controller' and make sure it is a
         # unique node (using anonymous=True).
         rospy.init_node('robot_control', anonymous=True)
 
-        # Publisher which will publish to the topic '/turtle1/cmd_vel'.
+        # Publisher which will publish a message of type 'Twist' to the topic '/turtle1/cmd_vel'.
         self.velocity_publisher = rospy.Publisher('/robot0/cmd_vel',
                                                   Twist, queue_size=10)
         
-        # A subscriber to the topic '/turtle1/pose'. self.update_pose is called
-        # when a message of type Pose is received.
+        # A subscriber to the topic '/turtle1/pose'. 
+        # self.update_pose is called when a message of type 'Odometry' is received.
         self.pose_subscriber = rospy.Subscriber('/robot0/odom',
                                                 Odometry, self.update_pose)
 
+        # Initialize pose variable (of type 'Pose') to store our current pose
         self.pose = Pose()
         
-        self.theta = 0
+        #Initialize variables to store the current x and y coordiantes and current orientation (theta)
         self.x = 0
         self.y = 0
+        self.theta = 0
+
+        # Set the distance and angle tolerance
+        self.distance_tolerance = 0.1
+        self.angle_tolerance = 5
+
         self.rate = rospy.Rate(10)
 
 
     def update_pose(self,data):
-        """Callback function which is called when a new message of type Pose is
+        """Callback function which is called when a new message of type Odometry is
         received by the subscriber."""
+        #Odomotry has a nested data structure. The pose is an attribute of pose with covariance 
         self.pose = data.pose.pose
-
         self.theta = 2 * atan2(self.pose.orientation.z, self.pose.orientation.w) * 180 / pi
         self.x = round(self.pose.position.x, 4)
         self.y = round(self.pose.position.y, 4)
-        print(self.x,self.y)
 
     def euclidean_distance(self,x,y):
         """Euclidean distance between current pose and the goal."""
         return sqrt(pow((x - self.x), 2) +
                     pow((y - self.y), 2))
+    #Works out the distance using pythagerous therom 
 
     def linear_vel(self,x,y, constant=1.5):
         """See video: https://www.youtube.com/watch?v=Qh15Nol5htM."""
         return constant * self.euclidean_distance(x,y)
+    #Multiplies distance by constant to increase speed of movement
 
     def steering_angle(self,x,y):
         """See video: https://www.youtube.com/watch?v=Qh15Nol5htM."""
         return atan2(y - self.y, x - self.x)
+    #Using tan in a triangle works out the direction to move
 
     def angular_vel(self,x,y, constant=6):
         """See video: https://www.youtube.com/watch?v=Qh15Nol5htM."""
         return constant * (self.steering_angle(x,y) - self.theta)
+    #Speed at rotation is done.
 
     def move2goal(self, waypoint):
         """Moves the turtle to the goal."""
 
-        # Get the input from the user.
-        x = int(waypoint[0])
-        y = int(waypoint[1])
 
-        distance_tolerance = 0.1
-        angle_tolerance = 5
+        # Set goal position and orientation to waypoint values
+        goal_x = int(waypoint[0])
+        goal_y = int(waypoint[1])
+        goal_theta = int(waypoint[2])
+
 
         vel_msg = Twist()
 
-        while self.euclidean_distance(x,y) >= distance_tolerance:
-            print(self.euclidean_distance(x,y))
+        while self.euclidean_distance(goal_x,goal_y) >= self.distance_tolerance:
 
             # Porportional controller.
             # https://en.wikipedia.org/wiki/Proportional_control
 
             # Linear velocity in the x-axis.
-            vel_msg.linear.x = self.linear_vel(x,y)
+            vel_msg.linear.x = self.linear_vel(goal_x,goal_y)
             vel_msg.linear.y = 0
             vel_msg.linear.z = 0
 
             # Angular velocity in the z-axis.
             vel_msg.angular.x = 0
             vel_msg.angular.y = 0
-            vel_msg.angular.z = 0
-            #vel_msg.angular.z = self.angular_vel(x,y)
+            vel_msg.angular.z = self.angular_vel(goal_x,goal_y)
 
             # Publishing our vel_msg
+            print(vel_msg)
             self.velocity_publisher.publish(vel_msg)
 
             # Publish at the desired rate.
             self.rate.sleep()
 
-        rospy.loginfo('Current position, x: {}, y:{}, theta:{}'.format(self.pose.position.x,
-                self.pose.position.y, self.theta))
-
-        rospy.spin()
+        
+        rospy.loginfo('Current position, x: {}, y:{}, theta:{}'.format(self.x,
+                self.y, self.theta))
 
         # Stopping our robot after the movement is over.
         vel_msg.linear.x = 0
         vel_msg.angular.z = 0
         self.velocity_publisher.publish(vel_msg)
 
+        while abs(goal_theta-self.theta)  >= self.angle_tolerance:
+
+            vel_msg.angular.z = (goal_theta-self.theta)*2
+            print(vel_msg)
+            self.velocity_publisher.publish(vel_msg)
+            self.rate.sleep()
+        
+        # Stopping our robot after the movement is over.
+        vel_msg.linear.x = 0
+        vel_msg.angular.z = 0
+        self.velocity_publisher.publish(vel_msg)
+
     # If we press control + C, the node will stop.
-   
+
 if __name__ == '__main__':
     try:
-        x = Robot()
-        # for waypoint in waypoints.values():
-        #     x.move2goal(waypoint)
-        #     rospy.spin()
-        #     rospy.sleep(1)
-        waypoint_1 = waypoints[1]
-        x.move2goal(waypoint)
+        x = Robot_controller()
+        #Works through set waypoints
+        for waypoint in waypoints.values():
+            x.move2goal(waypoint)
+            rospy.sleep(2)
+        rospy.spin()
+
     except rospy.ROSInterruptException:
         pass
-
